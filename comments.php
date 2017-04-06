203ae8a0d77951695a84203c1abf9311a1d56ffb
George Metaxas <gmetaxas@gmail.com>
2017-04-06 09:27:27 +0300

    Style comments.php and single.php

--- comments.php
+++ comments.php
@@ -18,6 +18,52 @@
 if ( post_password_required() ) {
 	return;
 }
+function sample_wp_theme_comment($comment, $args, $depth) {
+	if ('div' === $args ['style']) {
+		$tag = 'div';
+		$add_below = 'comment';
+	} else {
+		$tag = 'li';
+		$add_below = 'div-comment';
+	}
+	?>
+<<?php echo $tag ?>
+	<?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ) ?>
+	id="comment-<?php comment_ID() ?>">
+    <?php if ( 'div' != $args['style'] ) : ?>
+        <div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+    <?php endif; ?>
+    <div class="comment-author vcard">
+        <?php if ( $args['avatar_size'] != 0 ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+        <?php printf( __( '<cite class="fn">%s</cite> <span class="says">says:</span>' ), get_comment_author_link() ); ?>
+    </div>
+    <?php if ( $comment->comment_approved == '0' ) : ?>
+         <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em>
+	<br />
+    <?php endif; ?>
+
+    <div class="comment-meta commentmetadata">
+		<a
+			href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>">
+        <?php
+	/* translators: 1: date, 2: time */
+	printf ( __ ( '%1$s at %2$s' ), get_comment_date (), get_comment_time () );
+	?></a><?php
+	
+edit_comment_link ( __ ( '(Edit)' ), '  ', '' );
+	?>
+    </div>
+
+    <?php comment_text(); ?>
+
+    <div class="reply">
+        <?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+    </div>
+    <?php if ( 'div' != $args['style'] ) : ?>
+    </div>
+    <?php endif; ?>
+    <?php
+}
 ?>
 
 <div id="comments" class="comments-area">
@@ -33,39 +79,47 @@ if ( post_password_required() ) {
 					'<span>' . get_the_title() . '</span>'
 				);
 			?>
-		</h2><!-- .comments-title -->
+		</h2>
+	<!-- .comments-title -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'sample-wp-theme' ); ?></h2>
-			<div class="nav-links">
+		<nav id="comment-nav-above" class="navigation comment-navigation"
+		role="navigation">
+		<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'sample-wp-theme' ); ?></h2>
+		<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'sample-wp-theme' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'sample-wp-theme' ) ); ?></div>
+			<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'sample-wp-theme' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'sample-wp-theme' ) ); ?></div>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
+		</div>
+		<!-- .nav-links -->
+	</nav>
+	<!-- #comment-nav-above -->
 		<?php endif; // Check for comment navigation. ?>
 
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
 					'style'      => 'ol',
-					'short_ping' => true,
+					'callback' => 'sample_wp_theme_comment'
 				) );
 			?>
-		</ol><!-- .comment-list -->
+		</ol>
+	<!-- .comment-list -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'sample-wp-theme' ); ?></h2>
-			<div class="nav-links">
+		<nav id="comment-nav-below" class="navigation comment-navigation"
+		role="navigation">
+		<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'sample-wp-theme' ); ?></h2>
+		<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'sample-wp-theme' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'sample-wp-theme' ) ); ?></div>
+			<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'sample-wp-theme' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'sample-wp-theme' ) ); ?></div>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-below -->
+		</div>
+		<!-- .nav-links -->
+	</nav>
+	<!-- #comment-nav-below -->
 		<?php
 		endif; // Check for comment navigation.
 
@@ -82,4 +136,5 @@ if ( post_password_required() ) {
 	comment_form();
 	?>
 
-</div><!-- #comments -->
+</div>
+<!-- #comments -->
